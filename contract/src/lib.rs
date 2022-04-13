e528376869a8107370afa16da3d0ff72b2f48ada
Luciferius <twitter.cryptosketches@gmail.com>
2022-04-13 11:48:21 +0200

    Add first test for remember function.

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -2,8 +2,9 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
+    pub names: UnorderedMap<AccountId, String>,
 }
 
 #[near_bindgen]
@@ -11,18 +12,46 @@ impl Contract {
     #[init]
     pub fn new() -> Self {
         Self {
+            names: UnorderedMap::new(b"s".to_vec()),
         }
     }
     
+    pub fn hello(&mut self, name: String) -> String { 
+        format!("Hello {}!", name)
+    }
+    
     #[payable]
-    pub fn hello(&mut self, name: String) -> String {   
+    pub fn remember_me(&mut self, name: String) -> String {   
         if env::attached_deposit() != 0 {
             env::log_str(format!("Thanks for the {} NEAR.", env::attached_deposit()).as_str())
         }
         else {
-            //near_sdk::env::panic(b"Method do_not_take_my_money doesn't accept deposit");
+            near_sdk::env::panic(b"You need to deposit x NEAR in order to use this funtion. Storage is expensive...");
         }
+        self.names.insert(&env::predecessor_account_id(), &name);
         format!("Hello {}!", name)
+    }
+    
+    pub fn get_last_message(self, account_id: AccountId) -> Option<String> { 
+        let name = self.names.get(&account_id)
+        if name.is_some(){
+            format!("Hello {}!", name.unwrap())
+        }
+        else{
+            "I don't remember you."
+        }
+    }
+    
+    pub fn count_remembered_names(self, account_id: AccountId) -> u64 { 
+        self.names.len()
+    }
+    
+    pub fn get_updates(&self, from_index: u64, limit: u64) -> Vec<(AccountId, String)> {
+        let keys = self.names.keys_as_vector();
+        let values = self.names.values_as_vector();
+        (from_index..std::cmp::min(from_index + limit, self.names.len()))
+            .map(|index| (keys.get(index).unwrap(), values.get(index).unwrap()))
+            .collect()
     }
 }
 
@@ -62,5 +91,20 @@ mod tests {
         let result = contract.hello("Alice".to_string());
         assert_eq!(result, "Hello Alice!".to_string(), "Expected correct hello response.");
         //assert_eq!(get_logs(), ["Try again."], "Expected a failure log.");
+    }
+
+    #[test]
+    fn remember_me_says_hello() {
+        // Get Alice as an account ID
+        let alice = AccountId::new_unchecked("alice.testnet".to_string());
+        // Set up the testing context and unit test environment
+        let context = get_context(alice);
+        context.attached_deposit = 1000;
+        testing_env!(context.build());
+
+        // Set up contract object and call the new method
+        let mut contract = Contract::new();
+        let result = contract.remember_me("Alice".to_string());
+        assert_eq!(result, "Hello Alice!".to_string(), "Expected correct hello response.");
     }
 }
